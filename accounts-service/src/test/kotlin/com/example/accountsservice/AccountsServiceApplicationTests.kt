eb27ce2d2713b53cc6f84cb25795578d03f77d82
FoxcroftN <neilfoxcroftpro@gmail.com>
2024-04-19 20:44:12 +0200

    initialize project

---
+++ accounts-service/src/test/kotlin/com/example/accountsservice/AccountsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.accountsservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountsServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
