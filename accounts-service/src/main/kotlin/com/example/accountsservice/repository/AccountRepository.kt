e3c6fe6719c164ad3506a3038818c6ce7c517a98
FoxcroftN <neilfoxcroftpro@gmail.com>
2024-04-20 23:24:08 +0200

    change id from uuid to long to simplify

--- accounts-service/src/main/kotlin/com/example/accountsservice/repository/AccountRepository.kt
+++ accounts-service/src/main/kotlin/com/example/accountsservice/repository/AccountRepository.kt
@@ -1,9 +1,11 @@
 package com.example.accountsservice.repository
 
 import com.example.accountsservice.model.Account
-import java.util.UUID
+import java.util.Optional
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AccountRepository : CrudRepository<Account, UUID>
+interface AccountRepository : CrudRepository<Account, Long> {
+    fun findByUserName(username: String): Optional<Account>
+}
--- accounts-service/src/main/kotlin/com/example/accountsservice/repository/TransactionRepository.kt
+++ accounts-service/src/main/kotlin/com/example/accountsservice/repository/TransactionRepository.kt
@@ -1,10 +1,9 @@
 package com.example.accountsservice.repository
 
 import com.example.accountsservice.model.Transaction
-import java.util.UUID
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 
-interface TransactionRepository : CrudRepository<Transaction, UUID>
+interface TransactionRepository : CrudRepository<Transaction, Long>
